6aba062bc4f67b0b59db3675013f1ea866962a0b
domisProgrammingMemes <dominik.walczak@student.reutlingen-university.de>
2020-07-22 12:32:07 +0200

    random stuff

--- Tutorials.py
+++ Tutorials.py
@@ -2,6 +2,12 @@
 import torch
 print(torch.cuda.is_available())
 
+#########
+# place for  merge!
+
+
+#########
+
 # youtube tutorial: https://www.youtube.com/playlist?list=PLNmsVeXQZj7rx55Mai21reZtd_8m-qe27
 
 x = torch.Tensor(5,3)
