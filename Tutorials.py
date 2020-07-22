438a9a87c1d5a6f7a8498a48511942213cc43386
domisProgrammingMemes <dominik.walczak@student.reutlingen-university.de>
2020-07-22 12:38:29 +0200

    delete second tutorial because it was just a test

--- Tutorials.py
+++ Tutorials.py
@@ -2,15 +2,6 @@
 import torch
 print(torch.cuda.is_available())
 
-#########
-# place for  merge!
-# this will be the second Tutorial
-# some random comment
-z = torch.Tensor(1,1)
-print(z)
-
-#########
-
 # youtube tutorial: https://www.youtube.com/playlist?list=PLNmsVeXQZj7rx55Mai21reZtd_8m-qe27
 
 x = torch.Tensor(5,3)
