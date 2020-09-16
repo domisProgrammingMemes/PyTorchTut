1a00821ceaf74d7ae5d09ebb0d6685c44d93a6a3
domisProgrammingMemes <dominik.walczak@student.reutlingen-university.de>
2020-09-16 20:34:05 +0200

    "Reminder on Commits :D"

--- Tutorials.py
+++ Tutorials.py
@@ -3,6 +3,7 @@ import torch
 print(torch.cuda.is_available())
 
 # youtube tutorial: https://www.youtube.com/playlist?list=PLNmsVeXQZj7rx55Mai21reZtd_8m-qe27
+# Test comment
 
 x = torch.Tensor(5,3)
 print(x)
