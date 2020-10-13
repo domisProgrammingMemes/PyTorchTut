92a447296d4992a983ceb796ea1dc36be27cd8c7
domisProgrammingMemes <dominik.walczak@student.reutlingen-university.de>
2020-10-13 18:01:14 +0200

    clean up comments prep for second tutorial (MNIST)

---
+++ MNISTTutorial.py
@@ -0,0 +1,2 @@
+# youtube tutorial: https://www.youtube.com/playlist?list=PLNmsVeXQZj7rx55Mai21reZtd_8m-qe27
+# Test comment
--- WhatIsPyTorch.py
+++ WhatIsPyTorch.py
@@ -1,12 +1,7 @@
-# this will be the master-branch Tutorial
-import torch
-print(torch.cuda.is_available())
-
-# youtube tutorial: https://www.youtube.com/playlist?list=PLNmsVeXQZj7rx55Mai21reZtd_8m-qe27
-# Test comment
-
 # first Tutorial - What is PyTorch
 from __future__ import print_function
+import torch
+print(torch.cuda.is_available())
 # tensor 1x4 bedeuted: 1 Reihe, 4 Spalten (1 row 4 columns)
 
 
